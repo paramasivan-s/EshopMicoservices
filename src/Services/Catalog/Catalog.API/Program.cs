6d18b104d10b69941152088571dab1b2125f1eae
pownraj <pownraj@hotmail.com>
2025-11-13 16:49:24 +0400

    Commit after rearrage solution file

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config => 
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+}
+);
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}
+).UseLightweightSessions();
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+
+
+app.Run();
